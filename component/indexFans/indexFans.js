c593a81ba13e4019f9ed59cf14bd414a84ee596d
735940104@qq.com <735940104@qq.com>
2020-08-09 21:40:35 +0800

    wxapp up

---
+++ component/indexFans/indexFans.js
@@ -0,0 +1,55 @@
+// component/indexFans/indexFans.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    comicdata: [
+      {
+        img: "../../images/分区.png",
+        comicname: "钢之炼金术士天下第一yyds",
+        ep: "第五话"
+      },
+      {
+        img: "../../images/分区.png",
+        comicname: "emt",
+        ep: "第五话"
+      },
+      {
+        img: "../../images/分区.png",
+        comicname: "emt",
+        ep: "第五话"
+      },
+      {
+        img: "../../images/分区.png",
+        comicname: "emt",
+        ep: "第五话"
+      },
+      {
+        img: "../../images/分区.png",
+        comicname: "emt",
+        ep: "第五话"
+      },
+      {
+        img: "../../images/分区.png",
+        comicname: "emt",
+        ep: "第五话"
+      }
+    ]
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,90 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    videotext: [
+      {
+        img: "../../images/分区.png",
+        title: "起小点视频",
+        upName: "起小点",
+        watchCount: "111.1w",
+        date: "2222.1.1",
+        tag: "百万播放"
+      },
+      {
+        img: "../../images/我的.png",
+        title: "科普",
+        upName: "lex",
+        watchCount: "111.1w",
+        date: "2122.11.13",
+        tag: "百万播放"
+      },
+      {
+        img: "../../images/我的.png",
+        title: "科普",
+        upName: "lex",
+        watchCount: "111.1w",
+        date: "2122.11.13",
+        tag: "百万播放"
+      },
+      {
+        img: "../../images/我的.png",
+        title: "科普",
+        upName: "lex",
+        watchCount: "111.1w",
+        date: "2122.11.13",
+        tag: "百万播放"
+      },
+      {
+        img: "../../images/我的.png",
+        title: "科普",
+        upName: "lex",
+        watchCount: "111.1w",
+        date: "2122.11.13",
+        tag: "百万播放"
+      }
+      
+    ],
+    weekText: ['日', '一', '二', '三', '四', '五', '六'],
+    comicdata: [
+      {
+        img: "../../images/分区.png",
+        comicname: "钢之炼金术士天下第一yyds",
+        ep: "第五话"
+      },
+      {
+        img: "../../images/分区.png",
+        comicname: "emt",
+        ep: "第五话"
+      },
+      {
+        img: "../../images/分区.png",
+        comicname: "emt",
+        ep: "第五话"
+      },
+      {
+        img: "../../images/分区.png",
+        comicname: "emt",
+        ep: "第五话"
+      },
+      {
+        img: "../../images/分区.png",
+        comicname: "emt",
+        ep: "第五话"
+      },
+      {
+        img: "../../images/分区.png",
+        comicname: "emt",
+        ep: "第五话"
+      }
+    ]
+
+    
+  },
+  onLoad: function () {
+  },
+  getUserInfo: function(e) {
+  }
+})
